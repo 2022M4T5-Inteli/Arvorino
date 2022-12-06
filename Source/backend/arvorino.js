dd9325fec2d76185ec2dd6d94599c5c04d06c9fe
ipatriciahonorato <patricia.moreira@sou.inteli.edu.br>
2022-12-05 22:41:03 -0300

    Update Server

--- Source/backend/arvorino.js
+++ Source/backend/arvorino.js
@@ -21,45 +21,6 @@ const DBPATH = 'arvorino2.db'
 
 /* DEFINIÇÃO DOS ENDPOINTS */
 
-
-/* FETCH TEST POSTMAN API THINGSPEAK */
-setInterval(test, 60000)
-test();
-async function test() {
-	let response = await fetch("https://api.thingspeak.com/channels/1953984/feeds.json?minutes=1&results=1&timezone=America/Sao_Paulo") 
-	let data = await response.json()
-	for (let i = 0; i < data.feeds.length; i++) {
-		const element = data.feeds[i];
-		let sql = "INSERT INTO Teste (campo) VALUES ('" + element.field1 + "')";
-		var db = new sqlite3.Database(DBPATH);
-		db.run(sql, [], err => {
-			if (err) {
-				throw err;
-			}
-		});
-		db.close();
-	}
-}
-
-/* FETCH ESP32 DATA API THINGSPEAK */
-setInterval(test, 60000)
-test();
-async function test() {
-	let response = await fetch("https://api.thingspeak.com/channels/1963863/feeds.json?minutes=1&results=1&timezone=America/Sao_Paulo") 
-	let data = await response.json()
-	for (let i = 0; i < data.feeds.length; i++) {
-		const element = data.feeds[i];
-		let sql = "INSERT INTO Registros (temperatura, umidade, hora, minuto, dia, mes, id_estufa) VALUES ('" + element.field1 + "', '" + element.field2 + "', '" + element.field3 + "', '" + element.field4 + "', '" + element.field5 + "', '" + element.field6 + "', '" + element.field7 + "')";
-		var db = new sqlite3.Database(DBPATH);
-		db.run(sql, [], err => {
-			if (err) {
-				throw err;
-			}
-		});
-		db.close();
-	}
-}
-
 // NETWORKS - checar Registros cadastros na tabela NETWORK
 app.get('/registros', (req, res) => {
 	res.statusCode = 200;
@@ -210,3 +171,22 @@ app.listen(port, function () {
     console.log(`Listening on port ${port}`);
 });
 module.exports = app;
+
+/* FETCH ESP32-S3 DATA API THINGSPEAK PARA BANCO DE DADOS SQLTITE*/
+setInterval(test, 60000)
+test();
+async function test() {
+	let response = await fetch("https://api.thingspeak.com/channels/1963863/feeds.json?minutes=1&results=1&timezone=America/Sao_Paulo") 
+	let data = await response.json()
+	for (let i = 0; i < data.feeds.length; i++) {
+		const element = data.feeds[i];
+		let sql = "INSERT INTO Registros (temperatura, umidade, hora, minuto, dia, mes, id_estufa) VALUES ('" + element.field1 + "', '" + element.field2 + "', '" + element.field3 + "', '" + element.field4 + "', '" + element.field5 + "', '" + element.field6 + "', '" + element.field7 + "')";
+		var db = new sqlite3.Database(DBPATH);
+		db.run(sql, [], err => {
+			if (err) {
+				throw err;
+			}
+		});
+		db.close();
+	}
+}
